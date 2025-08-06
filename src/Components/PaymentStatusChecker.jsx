4cb207eccec541b2b667ec9391492646fffb69c1
UNIGO777 <naman13399@gmail.com>
2025-08-06 19:10:41 +0530

    aksjf

--- src/Components/PaymentStatusChecker.jsx
+++ src/Components/PaymentStatusChecker.jsx
@@ -11,7 +11,7 @@ const PaymentStatusChecker = ({ orderId, onSuccess }) => {
       setIsChecking(true);
       
       try {
-        const response = await fetch(`http://31.97.230.67:5001/api/orders/${orderId}`);
+        const response = await fetch(`https://api.palangtod.store//api/orders/${orderId}`);
         const data = await response.json();
         
         if (data.success && data.data) {
